38ed6d52f1ae543d437ae214fe32ac00abfbfb18
TechSammy <mrsam03102002@gmail.com>
2023-05-20 22:19:28 +0530

    connected firebase and done with the signin part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import SignIn from './SignIN'
+import LogOut from './LogOut';
+
+import { auth } from '../firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
 
 const style={
-    nav: `bg-gray-800 h-20 flex justify items-center`,
+    nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
     heading: `text-white text-3xl`
 }
 
 const Navbar = () => {
+    const [user] = useAuthState(auth)
+    console.log(user)
   return (
 <div className={style.nav}>
     <h1 className={style.heading}>Chat App</h1>
+    <SignIn/>
+    <LogOut/>
 </div>
   )
 }
